f8c8af43410ab72e7e081d8db6420f0402bbee44
dima <dimasilolil@gmail.com>
2020-12-19 18:06:29 +0600

    form dep

--- src/components/SignInForm/SigmForm.tsx
+++ src/components/SignInForm/SigmForm.tsx
@@ -1,4 +1,48 @@
 import React from "react";
-import { FormContainer } from "./styles";
+import { FormContainer, FormText, FormTextSign } from "./styles";
+import { Form, Field } from "react-final-form";
 
-export const SignInForm: React.FC = () => <FormContainer></FormContainer>;
+export const SignInForm: React.FC = () => (
+  <FormContainer>
+    <FormText>
+      <div>Welcome to BaseballCloud!</div>
+      <FormTextSign>Sign into your account here:</FormTextSign>
+    </FormText>
+    <Form
+      onSubmit={() => {}}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email</label>
+
+            {/* width: 100%;
+height: 50px;
+border-radius: 4px;
+background-color: #eff1f3;
+padding: 6px 12px 10px 37px;
+font-size: 16px;
+line-height: 1.13;
+font-weight: 400;
+color: #667784;
+border: 1px solid transparent; */}
+            <Field
+              name="firstName"
+              component="input"
+              type="text"
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <Field
+              name="password"
+              component="input"
+              type="text"
+              placeholder="Password"
+            />
+          </div>
+        </form>
+      )}
+    />
+  </FormContainer>
+);
--- src/components/SignInForm/styles.ts
+++ src/components/SignInForm/styles.ts
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const FormContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
   width: 100%;
   max-width: 450px;
   height: 400px;
@@ -10,4 +13,17 @@ export const FormContainer = styled.div`
   padding: 16px;
   border-radius: 8px;
   backdrop-filter: blur(5px);
+`;
+
+export const FormText = styled.div`
+  font-size: 24px;
+  line-height: 1.25;
+  font-weight: 400;
+  color: #667784;
+`;
+
+export const FormTextSign = styled.div`
+  text-align: center;
+  font-size: 16px;
+  margin: 8px;
 `;
